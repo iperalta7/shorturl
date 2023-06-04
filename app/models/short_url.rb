b20ab17bf93e4ecbcdeb135e232db72082f7d04e
Isaiah Peralta <iperalta@uri.edu>
2023-06-04 19:24:39 -0400

    add function to generate the short url on create

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,5 +1,14 @@
 class ShortUrl < ApplicationRecord
-end
+  before_validation :generate_short_url, on: :create
+
   validates :original_url, presence: true
   validates :short_url, presence: true, uniqueness: true
+
+  def generate_short_url
+    self.short_url = loop do
+      random_token = SecureRandom.urlsafe_base64(3, false)
+      break random_token unless ShortUrl.exists?(short_url: random_token)
+    end
+  end
+  
 end
